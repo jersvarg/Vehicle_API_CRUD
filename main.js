a8873ad811c6f72ed787a4401e15cc1708a3a1b6
jers0nvargas <jers0nvargas@outlook.com>
2022-11-29 21:08:13 -0500

    CRUD METODOS COMPLETOS

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ var app= new Vue({
             Precio:"",
             Modelo:"",
             Cilindraje:""
-        }
+        },
+        dataEdit:null,
+        isEdit:false,
     },
     mounted() {
         axios.get("http://127.0.0.1:8000/")
@@ -18,11 +20,42 @@ var app= new Vue({
     },
     methods: {
         onSubmit(){
-            axios.post('http://127.0.0.1:8000/',this.form)
-            .then(res=>{
+            if(this.isEdit){
+                this.isEdit=false;
+                axios.put("http://127.0.0.1:8000/"+this.dataEdit._id,this.form)
+                .then(res=>{
+                    this.isEdit=false;
+                    location.reload();
+                });
+            }
+            else{
+                axios.post('http://127.0.0.1:8000/',this.form)
+                .then(res=>{
                 alert("Se agrega un Auto")
                 location.reload();
             })
-        }
-    },
+            }            
+        },
+        onDelete_auto(auto){
+            axios.delete('http://127.0.0.1:8000/'+auto._id)
+            .then(res=>{                
+                location.reload();
+            })
+            console.log(auto)
+        },
+        onEdit_auto(auto){
+            /*axios.put('http://127.0.0.1:8000/'+auto._id)
+            .then(res=>{                
+                location.reload();
+            })*/
+            this.form.Marca=auto.Marca;
+            this.form.Color=auto.Color;
+            this.form.Precio=auto.Precio;
+            this.form.Modelo=auto.Modelo;
+            this.form.Cilindraje=auto.Cilindraje;
+            this.dataEdit=auto;
+            this.isEdit=true
+            //console.log(auto) muestra el auto selecionado en consola (html)
+        }        
+    },    
 })
